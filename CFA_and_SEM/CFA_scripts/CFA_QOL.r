fca027e9db7e593cebd5a9bbd392fad2d7a0b822
Oriel Singer <oriel2486@gmail.com>
2024-04-15 14:15:07 +0300

    update of qol file. it is more updated then remote version, but either of the two is not the full version i'm looking for.

--- CFA_and_SEM/CFA_scripts/CFA_QOL.r
+++ CFA_and_SEM/CFA_scripts/CFA_QOL.r
@@ -25,66 +25,43 @@ respondents_data <- read_excel(data_file)
 results_file <- file.path(results_dir, "CFA_qol.txt")
 
 ################################################################################
+# y3 + y9 func
+# y12 comp
+# y1 and y8 are in func instead of pos
 
-# To save all results
-sink(results_file)
-
-# Read the loadings and their mappings
-qol_loadings <- read.csv(file.path(current_dir, "..", "loadings/QOL_loadings.csv"), stringsAsFactors = FALSE)
-y_mapping <- read.csv(file.path(current_dir, "..", "items_maps/y_mapping.csv"), stringsAsFactors = FALSE)
-
-# Function to find the row indices where each factor has the maximum value
-get_max_indices_per_factor <- function(df) {
-  apply(df, 1, function(x) which(x == max(x)))
-}
-
-# Function to find the row indices where each factor takes items above threshold
-get_indices_above_threshold <- function(df, threshold = 0.3) {
-  # Create a list to store indices for each factor
-  factor_indices_list <- vector("list", ncol(df))
-  names(factor_indices_list) <- colnames(df)
-  
-  for (factor in seq_along(factor_indices_list)) {
-    factor_indices_list[[factor]] <- which(abs(df[, factor]) >= threshold)
-  }
-  
-  return(factor_indices_list)
-}
-
-qol_max_indices <- get_max_indices_per_factor(qol_loadings[, c("Factor1", "Factor2", "Factor3")])
-qol_threshold_indices <- get_indices_above_threshold(qol_loadings[, c("Factor1", "Factor2", "Factor3")], 0.3)
-
-# Create lists for each factor
-#QOL1_indices <- which(qol_max_indices == 1)
-#QOL2_indices <- which(qol_max_indices == 2)
-#QOL3_indices <- which(qol_max_indices == 3)
-QOL1_indices <- qol_threshold_indices[["Factor1"]]
-QOL2_indices <- qol_threshold_indices[["Factor2"]]
-QOL3_indices <- qol_threshold_indices[["Factor3"]]
-
-# Map indices to letter (y,x ...) variables
-map_indices <- function(indices, mapping_df) {
-  mapping_df$Variable[indices]
-}
-
-qol1_y <- map_indices(QOL1_indices, y_mapping)
-qol2_y <- map_indices(QOL2_indices, y_mapping)
-qol3_y <- map_indices(QOL3_indices, y_mapping)
+QOL.model <- 	'  
+          functioning =~ y10 + y1 +  y4 + y5 + y6 + y8 +  y11 + y13 + y14
+			    competence =~  y16 + y2 + y15 + y17 + y18
+			    positive_feeling =~ y21 + y7  + y19 + y20  +  y22
+			    qol =~ functioning + competence + positive_feeling
+				'
+QOL.fit <- cfa(QOL.model, data = respondents_data)
+summary(QOL.fit, standardized = TRUE, fit.measures = TRUE)
 
 
-full.model2 <- paste(
-  "qol1 =~", paste(qol1_y, collapse = " + "), ";",
-  "qol2 =~", paste(qol2_y, collapse = " + "), ";",
-  "qol3 =~", paste(qol3_y, collapse = " + "), ";",
-  "qol =~ qol1 + qol2 + qol3;",
-   sep = "\n"
-)
+################################################################################
+# y3 + y9 func
+# y12 comp
+# y1 in func instead of pos
+## y1 in pos as original
+QOL.model <- 	'  
+          functioning =~ y10 +  y4 + y5 + y6 + y8 +  y11 + y13 + y14
+			    competence =~  y16 + y2 + y15 + y17 + y18
+			    positive_feeling =~ y21 + y7  + y19 + y20  +  y22 + y1
+			    qol =~ functioning + competence + positive_feeling
+				'
+QOL.fit <- cfa(QOL.model, data = respondents_data)
+summary(QOL.fit, standardized = TRUE, fit.measures = TRUE)
 
-
-full.fit2 <- cfa(full.model2, data = respondents_data)
-summary(full.fit2, standardized = TRUE, fit.measures = TRUE)
-
-# Save the results to the output file
-sink()
-
-semPaths(full.fit2, intercepts = TRUE, edge.label.cex = 1, what = 'std', structural = FALSE)
+################################################################################
+# y3 + y9 func
+# y12 comp
+## y1 and y8 in pos as original ## NOT LEGIT - negative lv. 
+QOL.model <- 	'  
+          functioning =~ y10 +  y4 + y5 + y6 +  y11 + y13 + y14
+			    competence =~  y16 + y2 + y15 + y17 + y18
+			    positive_feeling =~ y21 + y7  + y19 + y20  +  y22 + y1 + y8
+			    qol =~ functioning + competence + positive_feeling
+				'
+QOL.fit <- cfa(QOL.model, data = respondents_data)
+summary(QOL.fit, standardized = TRUE, fit.measures = TRUE)
